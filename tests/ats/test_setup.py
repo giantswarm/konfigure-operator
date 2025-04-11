f91691633df4f26276ee50c499d6fe2c9e7e0edc
Laszlo Uveges <laszlo@giantswarm.io>
2025-04-11 10:17:12 +0200

    Remove obsolete file

--- tests/ats/test_setup.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from pytest_helm_charts.clusters import Cluster
-
-
-@pytest.fixture(scope='module')
-def fixtures(kube_cluster: Cluster):
-    print("This should be invoked once per module!")
