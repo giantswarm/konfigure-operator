4be265a743ecc9e2f34c0262c742aaec012dc24c
Laszlo Uveges <laszlo@giantswarm.io>
2025-07-29 10:42:09 +0200

    Scaffold Konfiguration CRD and controller with kube-builder

--- main.go
+++ main.go
@@ -170,6 +170,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ManagementClusterConfiguration")
 		os.Exit(1)
 	}
+	if err = (&controller.KonfigurationReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Konfiguration")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
